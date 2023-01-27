7e00f17a94cbc837164d07e282e7d062719912dd
Baljeet <baljeetkhalsha425@gmail.com>
2023-01-27 15:55:39 +0530

    changes

--- src/app/layout/customer-leads/logs/logs.component.ts
+++ src/app/layout/customer-leads/logs/logs.component.ts
@@ -5,6 +5,7 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 import { CommonService } from '../../../services/commonService/common.service';
 import { ApiUrl } from '../../../core/apiUrl';
 import { HttpService } from '../../../services/http/http.service';
+import { PaginationControls1 } from '../../../shared/models/pagination-model';
 
 @Component({
   selector: 'app-add-account',
@@ -14,6 +15,7 @@ export class LogsComponent implements OnInit {
 
   public onClose: Subject<{}> = new Subject();
   modalData: any;
+  pagination1 = new PaginationControls1()
   allData: any = [];
 
   constructor(public message: MessageService, public bsModalRef: BsModalRef, public commonService: CommonService,
@@ -25,11 +27,15 @@ export class LogsComponent implements OnInit {
   }
 
   getData() {
+    this.pagination1.skip = (this.pagination1.pageNo - 1) * this.pagination1.limit;
     let obj: any = {
+      skip: this.pagination1.skip,
       _id: this.modalData._id
     };
     this.http.getData(ApiUrl.list_lead_logs, obj).subscribe(res => {
       this.allData = res.data.data;
+      this.pagination1.count = res.data.total_count;
+      console.log(this.pagination1.count, 'this.pagination.count');
     }, () => {
     });
   }
--- src/app/shared/models/pagination-model.ts
+++ src/app/shared/models/pagination-model.ts
@@ -7,3 +7,13 @@ export class PaginationControls {
   directionLinks = true;
   rotate = true;
 }
+
+export class PaginationControls1 {
+  limit = 10;
+  pageSize = 10;
+  skip = 0;
+  count = 0;
+  pageNo = 1;
+  directionLinks = true;
+  rotate = true;
+}
